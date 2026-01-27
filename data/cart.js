2afa716712549cdf5ebfb5668a8b9b19c16193c8
jypark5 <justinpark3105@gmail.com>
2026-01-27 20:29:43 +1100

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
 }, {
@@ -16,4 +16,9 @@ export const addToCart = (productId) => {
       quantity: 1
     });
   }
+}
+
+export const removeFromCart = (productId) => {
+  const indexToRemove = cart.findIndex(item => item.productId === productId);
+  cart.splice(indexToRemove, 1);
 }
